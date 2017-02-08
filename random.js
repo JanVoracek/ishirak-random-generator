e7b3ed3033675fcc8ad8e6824a736f90c14b1056
Jan Voráček <jan@voracek.net>
2017-02-08 22:42:24 +0100

    Implemented random generator

---
+++ random.js
@@ -0,0 +1,18 @@
+const words = [
+  'Zajíc',
+  'Kur',
+  'Vepř',
+  'Prase',
+  'Květák',
+  'Bla',
+  'Kra',
+  'Vla',
+  'Slepice',
+  'Ahoj',
+  'Banán',
+  'Jablko',
+  'Pecka',
+];
+
+const randomWord = words[Math.floor(Math.random() * words.length)];
+console.log(randomWord);
